f5b44f735bc78d53a9dc365a57e5c39497a031c6
biubiuy <2905717839@qq.com>
2019-12-18 06:37:29 -0800

    0

---
+++ lab5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<stdio.h>
+
+void reverse(char s[])
+{	if(s[0]!='\0')
+	{	reverse(s+1);
+		printf("%c",*s);
+	}
+}
+main()
+{
+	char s[200]={'\0'};
+	printf("请输入一个字符串：\n");
+	gets(s);
+	reverse(s);
+	printf("\n");
+
+}
